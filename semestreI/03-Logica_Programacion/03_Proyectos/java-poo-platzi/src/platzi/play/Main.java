5fc457161cd0f7bc8ee4f4bdaeecf51a87414f9d
aserrador <alejandro.ayala@onecluster.org>
2026-03-24 17:03:45 -0500

    update courses

--- semestreI/03-Logica_Programacion/03_Proyectos/java-poo-platzi/src/platzi/play/Main.java
+++ semestreI/03-Logica_Programacion/03_Proyectos/java-poo-platzi/src/platzi/play/Main.java
@@ -1,4 +1,17 @@
 package platzi.play;
 
+import platzi.play.contenido.Pelicula;
+
 public class Main {
+    public static void main(String[] args){
+        System.out.println("PLATZI PLAY");
+
+        Pelicula pelicula = new Pelicula();
+        pelicula.titulo = "Harry Popote";
+        pelicula.anioEstreno = 2001;
+        pelicula.genero = "Fantasia";
+        pelicula.calificar(5.0);
+
+        System.out.println(pelicula.obtenerFichaTecnica());
+    }
 }
--- semestreI/03-Logica_Programacion/03_Proyectos/java-poo-platzi/src/platzi/play/contenido/Pelicula.java
+++ semestreI/03-Logica_Programacion/03_Proyectos/java-poo-platzi/src/platzi/play/contenido/Pelicula.java
@@ -1,4 +1,26 @@
 package platzi.play.contenido;
 
 public class Pelicula {
+    public String titulo;
+    public String description;
+    public int duracion;
+    public String genero;
+    public int anioEstreno;
+    public double calificacion;
+    public boolean disponible;
+
+    public void reproducir(){
+        System.out.println("Reproduciendo " + titulo);
+    }
+
+    public String obtenerFichaTecnica(){
+        return titulo + " (" + anioEstreno + ")\n" +
+                "Genero: " + genero + "\n" + "Calificacion: " + calificacion + "/5";
+    }
+
+    public void calificar(double calificacion) {
+        if(calificacion >=0 && calificacion <=5){
+            this.calificacion = calificacion;
+        }
+    }
 }
